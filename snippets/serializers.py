1a2c2008ee82ed5bc9c7fc894dbb13d006f0948c
easywaldo <easywaldoji@gmail.com>
2022-02-18 23:59:14 +0900

    User 에 대한 Create 기능 추가

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
 # class SnippetSerializer(serializers.Serializer):
@@ -32,3 +33,10 @@ class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
--- snippets/views.py
+++ snippets/views.py
@@ -16,6 +16,9 @@ from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
 
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
 # Create your views here.
 # @csrf_exempt
 # def snippet_list(request):
@@ -156,3 +159,12 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
