0ce15ba3a76e85b81a7cc2ed7a8c310585f0b547
easywaldo <easywaldoji@gmail.com>
2022-02-21 22:35:59 +0900

    SwaggerUI modified
    /swagger/?format=openapi url applied

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -20,6 +20,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.generic import TemplateView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -39,6 +40,10 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
 #urlpatterns = format_suffix_patterns(urlpatterns)
 
