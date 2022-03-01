7bcc74b0cd43e97963a9b1f0cbf6585cc7be578d
jinam.lee <jinam.lee@a-bly.com>
2022-03-01 17:00:28 +0900

    MyJsonEncoder 를 추가한다
    DjangoJSONEncoder 를 상속받아 객체를 직렬화 하는 커스텀클래스를 작성한다.

---
+++ tutorial/my_json_encoder.py
@@ -0,0 +1,19 @@
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models.query import QuerySet
+from django.contrib.auth.models import User
+
+class MyJsonEncoder(DjangoJSONEncoder):
+    """
+    from tutorial.my_json_encoder import MyJsonEncoder
+    from django.contrib.auth.models import User
+    import json
+    data = User.objects.all()
+    json.dumps(data, cls=MyJsonEncoder, ensure_ascii=False)
+    '[{"id": 1}]'
+    """
+    def default(self, obj):
+        if isinstance(obj, QuerySet):
+            return tuple(obj)
+        elif isinstance(obj, User):
+            return {'id': obj.id}
+        return super().default(obj)
